a6c316e3f5f8ae87d8ebd97661e4646f6ecc5971
Katuhito Hara <katuhitohara@gmail.com>
2020-10-27 16:07:18 +0900

    POST Usability

--- archive.php
+++ archive.php
@@ -12,6 +12,15 @@ if ( have_posts() ):
 endif;
 ?>
                       </div>
+                      <div class="pager">
+                        <ul class="pageList">
+<?php
+if ( function_exists( 'page_navi' )):
+  page_navi();
+endif;
+?>
+                        </ul>
+                      </div>
                     </div>
                   </div>
                 </div>
--- header.php
+++ header.php
@@ -35,9 +35,9 @@
             );
             ?>
           </nav>
-          <form class="search-form" role="search" method="get" action="">
+          <form class="search-form" role="search" method="get" action="<?php echo esc_url( home_url() ); ?>">
             <div class="search-box">
-              <input type="text" class="search-input" name="" placeholder="キーワードを入力してください" />
+              <input type="text" class="search-input" name="s" placeholder="キーワードを入力してください" />
               <button type="submit" class="button-submit"></button>
             </div>
             <div class="search-buttons">
@@ -73,4 +73,9 @@
               </div>
             </div>
             <div class="page-container">
-    <?php endif; ?>
+<?php
+if ( function_exists('bread_crumb') ):
+  bread_crumb();
+endif;
+?>
+<?php endif; ?>
